8ea5eb0b67e7261bae02d2477cf68d581d88e0c9
thesmalllest <vikulik.vs.137@gmail.com>
2024-09-27 17:55:34 +0300

    lab 1 | task 2
    Дополнила функционал программы:
    1. получаю имя пользователя как аргумент программы;
    2. спрашиваю у пользователя какой у него любимый язык программирования.

--- introduction_to_ruby/introduction_with_user.rb
+++ introduction_to_ruby/introduction_with_user.rb
@@ -1,1 +1,24 @@
 puts "Hello World!" 
+
+user_name = ARGV[0] 
+puts "Добро пожаловать, #{user_name}!" 
+
+puts "Какой твой любимый язык программирования?"
+favorite_language = STDIN.gets.chomp
+
+if favorite_language.downcase == "ruby"
+  puts "Ты, конечно, подлиза!"
+else
+  puts "Скоро будет Ruby!"
+  # Комментарии для разных языков:
+  case favorite_language.downcase
+  when "python"
+    puts "Python тоже отличный язык!"
+  when "javascript"
+    puts "JavaScript - язык веб-разработки!"
+  when "java"
+    puts "Java - мощный язык для больших проектов!"
+  else
+    puts "Любой язык хорош, если в нем писать хороший код!"
+  end
+end
