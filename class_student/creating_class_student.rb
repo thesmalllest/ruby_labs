a1b2b6db61e86059ae97fd399fa9fb9eee45d004
thesmalllest <vikulik.vs.137@gmail.com>
2024-10-12 01:14:18 +0300

    lab 2 | task 1.5
    Теперь необязательные поля можно заполнять любой комбинацией

--- class_student/creating_class_student.rb
+++ class_student/creating_class_student.rb
@@ -1,7 +1,7 @@
 class Student
  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
- def initialize(id, surname, name, patronymic, phone, telegram, email, git)
+ def initialize(id, surname, name, patronymic, phone = nil, telegram = nil, email = nil, git = nil)
   @id = id
   @surname = surname
   @name = name
@@ -15,5 +15,4 @@ class Student
  def to_s
   "ID: #{id}: ФИО: #{surname} #{name} #{patronymic} Телефон: #{phone} Telegram: #{telegram} Email: #{email} Git: #{git}"
  end
-
 end
