20d66ee580245773a6e3f3576495e5c2e03e53ad
thesmalllest <vikulik.vs.137@gmail.com>
2024-10-12 00:54:07 +0300

    lab 2 | task 1.3 1.4
    Чтобы избежать дублирования кода в конструкторе, геттере и сеттере, воспользовалась attr_accessor

--- class_student/creating_class_student.rb
+++ class_student/creating_class_student.rb
@@ -1,5 +1,7 @@
 class Student
- def initialize(id, surname, name, patronymic, phone = nil, telegram = nil, email = nil, git = nil)
+ attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+
+ def initialize(id, surname, name, patronymic, phone, telegram, email, git)
   @id = id
   @surname = surname
   @name = name
@@ -8,72 +10,6 @@ class Student
   @telegram = telegram
   @email = email
   @git = git
- end
-
- # Геттеры
- def id
-  @id
- end
-
- def surname
-  @surname
- end
-
- def name
-  @name
- end
-
- def patronymic
-  @patronymic
- end
-
- def phone
-  @phone
- end
-
- def telegram
-  @telegram
- end
-
- def email
-  @email
- end
-
- def git
-  @git
- end
-
- # Сеттеры
- def id=(new_id)
-  @id = new_id
- end
-
- def surname=(new_surname)
-  @surname = new_surname
- end
-
- def name=(new_name)
-  @name = new_name
- end
-
- def patronymic=(new_patronymic)
-  @patronymic = new_patronymic
- end
-
- def phone=(new_phone)
-  @phone = new_phone
- end
-
- def telegram=(new_telegram)
-  @telegram = new_telegram
- end
-
- def email=(new_email)
-  @email = new_email
- end
-
- def git=(new_git)
-  @git = new_git
  end
 
  def to_s
