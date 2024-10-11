71938e5ba4384e2cecb1a35f29ea1debb3ea724d
thesmalllest <vikulik.vs.137@gmail.com>
2024-10-11 22:33:26 +0300

    lab 2 | task 1.1
    Создала класс Student. Написала конструктор, геттеры и сеттеры для каждого поля

---
+++ class_student/creating_class_student.rb
@@ -0,0 +1,78 @@
+class Student
+ def initialize(id, surname, name, patronymic, phone = nil, telegram = nil, email = nil, git = nil)
+  @id = id
+  @surname = surname
+  @name = name
+  @patronymic = patronymic
+  @phone = phone
+  @telegram = telegram
+  @email = email
+  @git = git
+ end
+
+ # Геттеры
+ def id
+  @id
+ end
+
+ def surname
+  @surname
+ end
+
+ def name
+  @name
+ end
+
+ def patronymic
+  @patronymic
+ end
+
+ def phone
+  @phone
+ end
+
+ def telegram
+  @telegram
+ end
+
+ def email
+  @email
+ end
+
+ def git
+  @git
+ end
+
+ # Сеттеры
+ def id=(new_id)
+  @id = new_id
+ end
+
+ def surname=(new_surname)
+  @surname = new_surname
+ end
+
+ def name=(new_name)
+  @name = new_name
+ end
+
+ def patronymic=(new_patronymic)
+  @patronymic = new_patronymic
+ end
+
+ def phone=(new_phone)
+  @phone = new_phone
+ end
+
+ def telegram=(new_telegram)
+  @telegram = new_telegram
+ end
+
+ def email=(new_email)
+  @email = new_email
+ end
+
+ def git=(new_git)
+  @git = new_git
+ end
+end
