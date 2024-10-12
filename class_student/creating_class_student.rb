3c68c4c614546905a2496173a8837f10072c1a53
thesmalllest <vikulik.vs.137@gmail.com>
2024-10-12 13:05:23 +0300

    lab 2 | task 1.8
    Создать валидации для корректной формы строки в
    остальных полях.

--- class_student/creating_class_student.rb
+++ class_student/creating_class_student.rb
@@ -2,9 +2,7 @@ class Student
  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
  def initialize(student_data)
-
-  raise ArgumentError, "Некорректный номер телефона" unless Student.valid_phone(student_data[:phone])
-
+  validate_data(student_data)
   @id = student_data[:id]
   @surname = student_data[:surname]
   @name = student_data[:name]
@@ -15,12 +13,43 @@ class Student
   @git = student_data[:git]
  end
 
- def self.valid_phone(phone)
+ def to_s
+  "ID: #{@id}: ФИО: #{@surname} #{@name} #{@patronymic} Телефон: #{@phone} Telegram: #{@telegram} Email: #{@email} Git: #{@git}"
+ end
+
+ def self.valid_fio?(surname, name, patronymic)
+  return false unless surname.is_a?(String) && name.is_a?(String) && patronymic.is_a?(String)
+  return false unless surname.match?(/^[А-Яа-я]+$/) && name.match?(/^[А-Яа-я]+$/) && patronymic.match?(/^[А-Яа-я]+$/)
+  true
+ end
+ 
+ def self.valid_phone?(phone)
   return false unless phone.is_a?(String)
   phone.match?(/^(?:\+7|8)\d{10}$/)
  end
 
- def to_s
-  "ID: #{id}: ФИО: #{surname} #{name} #{patronymic} Телефон: #{phone} Telegram: #{telegram} Email: #{email} Git: #{git}"
+ def self.valid_telegram?(telegram)
+  return false unless telegram.is_a?(String)
+  telegram.match?(/^[a-zA-Z0-9_]{5,32}$/)
+ end
+
+ def self.valid_email?(email)
+  return false unless email.is_a?(String)
+  email.match?(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+ end
+
+ def self.valid_git?(git)
+  return false unless git.is_a?(String)
+  git.match?(/^[a-zA-Z0-9_]+$/)
+ end
+
+ private
+
+ def validate_data(student_data)
+  raise ArgumentError, "Некорректное ФИО" unless Student.valid_fio?(student_data[:surname], student_data[:name], student_data[:patronymic]) 
+  raise ArgumentError, "Некорректный номер телефона" if student_data.key?(:phone) && !Student.valid_phone?(student_data[:phone])
+  raise ArgumentError, "Некорректное имя пользователя Telegram" if student_data.key?(:telegram) && !Student.valid_telegram?(student_data[:telegram])
+  raise ArgumentError, "Некорректный email" if student_data.key?(:email) && !Student.valid_email?(student_data[:email]) 
+  raise ArgumentError, "Некорректное имя пользователя Git" if student_data.key?(:git) && !Student.valid_git?(student_data[:git])
  end
 end
--- class_student/main.rb
+++ class_student/main.rb
@@ -19,16 +19,53 @@ student_data_2 = {
  phone: '88765432144'
 }
 
+student_data_3 = {
+ id: 3,
+ surname: 'Пушкин',
+ name: 'Александр',
+ patronymic: 'Сергеевич',
+ phone: '+79112345678',
+ telegram: 'pushkin_tg',
+ email: 'alexandr.pushkin@example.com',
+ git: 'pushkin-git' 
+}
+
+student_data_4 = {
+ id: 4,
+ surname: 'Чехов',
+ name: 'Anton',
+ patronymic: 'Павлович',
+ phone: '89112345678',
+ telegram: 'anton_chehov',
+ email: 'anton.chehov@example.com',
+ git: 'chekhov'
+}
+
+
 begin
- student1 = Student.new(student_data_1)
+ student_1 = Student.new(student_data_1)
  puts "Студент 1 создан успешно: #{student1}"
 rescue ArgumentError => e
  puts "Ошибка при создании студента 1: #{e.message}"
 end
 
 begin
- student2 = Student.new(student_data_2)
- puts "Студент 2 создан успешно: #{student2}"
+ student_2 = Student.new(student_data_2)
+ puts "Студент 2 создан успешно: #{student_2}"
 rescue ArgumentError => e
  puts "Ошибка при создании студента 2: #{e.message}"
+end
+
+begin
+ student_3 = Student.new(student_data_3)
+ puts "Студент 3 создан успешно: #{student_3}"
+rescue ArgumentError => e
+ puts "Ошибка при создании студента 3: #{e.message}"
+end
+
+begin
+ student_4 = Student.new(student_data_4)
+ puts "Студент 4 создан успешно: #{student_4}"
+rescue ArgumentError => e
+ puts "Ошибка при создании студента 4: #{e.message}"
 end
