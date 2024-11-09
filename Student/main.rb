53dcebe05f4d7f6867c2bd970f01090fc5328cf6
thesmalllest <vikulik.vs.137@gmail.com>
2024-11-09 13:29:18 +0300

    lab 2 | task 1.7
    Добавлен метод валидации для номера телефона.

--- Student/main.rb
+++ Student/main.rb
@@ -5,7 +5,7 @@ student_1 = Student.new({
   name: "Сергей",
   patronymic: "Александрович",
   id: 1,
-  phone: "123456789",
+  phone: "+79231233242",
   telegram: "@sergey_esenin",
   email: "sergey.esenin@example.com",
   git: "eseninss"
@@ -13,4 +13,4 @@ student_1 = Student.new({
 
 
 puts "Информация о студенте 1:"
-puts student_1.to_s
+puts student_1
--- Student/student.rb
+++ Student/student.rb
@@ -2,18 +2,28 @@ class Student
   attr_accessor :id, :phone, :telegram, :email, :git
   attr_reader :surname, :name, :patronymic
 
-  def initialize(student_data = {})
+  def initialize(student_data = {surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil})
     @surname = student_data[:surname]
     @name = student_data[:name]
     @patronymic = student_data[:patronymic]
     @id = student_data[:id]
-    @phone = student_data[:phone]
+    self.phone = student_data[:phone]
     @telegram = student_data[:telegram]
     @email = student_data[:email]
     @git = student_data[:git]
   end
 
+  def self.valid_phone?(phone)
+    phone.match(/\A(\+7|8)?\d{10}\z/)
+  end
+
+  def phone=(phone)
+  		raise "Некорректный номер телефона" if !Student.valid_phone?(phone)
+  		@phone=phone
+  end
+
   def to_s
     "ID: #{id}, ФИО: #{surname} #{name} #{patronymic}, Телефон: #{phone}, Telegram: #{telegram}, Email: #{email}, Git: #{git}"
   end
+
 end
