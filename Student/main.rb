2acbaa8282bdbdf588c9acf2df5732b78e93812b
thesmalllest <vikulik.vs.137@gmail.com>
2024-11-09 11:52:19 +0300

    lab 2 | task 1.6
    Аргументы конструктора теперь в форме ХЭШа.

--- Student/main.rb
+++ Student/main.rb
@@ -1,6 +1,16 @@
 require_relative 'student'
 
-student_1 = Student.new('Есенин', 'Сергей', 'Александрович', 1, '1234567890', 'sergey_esenin', 'sergey.esenin@example.com', 'eseninss')
+student_1 = Student.new({
+  surname: "Есенин",
+  name: "Сергей",
+  patronymic: "Александрович",
+  id: 1,
+  phone: "123456789",
+  telegram: "@sergey_esenin",
+  email: "sergey.esenin@example.com",
+  git: "eseninss"
+})
+
 
 puts "Информация о студенте 1:"
 puts student_1.to_s
--- Student/student.rb
+++ Student/student.rb
@@ -1,23 +1,19 @@
 class Student
+  attr_accessor :id, :phone, :telegram, :email, :git
+  attr_reader :surname, :name, :patronymic
 
-	attr_accessor :id, :phone, :telegram, :email, :git
-	attr_reader :surname, :name, :patronymic
+  def initialize(student_data = {})
+    @surname = student_data[:surname]
+    @name = student_data[:name]
+    @patronymic = student_data[:patronymic]
+    @id = student_data[:id]
+    @phone = student_data[:phone]
+    @telegram = student_data[:telegram]
+    @email = student_data[:email]
+    @git = student_data[:git]
+  end
 
-	def initialize(surname, name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
-	
-		@surname = surname
-		@name = name
-		@patronymic = patronymic
-		@id = id
-		@phone = phone
-		@telegram = telegram
-		@email = email
-		@git = git
-
-	end
-
-	def to_s
-		"ФИО: #{surname} #{name} #{patronymic}, ID: #{id}, Телефон: #{phone}, Telegram: #{telegram}, Email: #{email}, Git: #{git}"
-	end
-
+  def to_s
+    "ID: #{id}, ФИО: #{surname} #{name} #{patronymic}, Телефон: #{phone}, Telegram: #{telegram}, Email: #{email}, Git: #{git}"
+  end
 end
