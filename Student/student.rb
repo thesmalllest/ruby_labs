8a1276910aae9e709124d5606cf6e4ead2c3774a
thesmalllest <vikulik.vs.137@gmail.com>
2024-11-11 13:52:13 +0300

    lab 2 | task 1.8
    Добавила валидации для остальных полей.

--- Student/student.rb
+++ Student/student.rb
@@ -1,16 +1,51 @@
 class Student
-  attr_accessor :id, :phone, :telegram, :email, :git
-  attr_reader :surname, :name, :patronymic
+  attr_reader :surname, :name, :patronymic, :id, :phone, :telegram, :email, :git
 
   def initialize(student_data = {surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil})
-    @surname = student_data[:surname]
-    @name = student_data[:name]
-    @patronymic = student_data[:patronymic]
-    @id = student_data[:id]
+    self.surname = student_data[:surname]
+    self.name = student_data[:name]
+    self.patronymic = student_data[:patronymic]
+    self.id = student_data[:id]
     self.phone = student_data[:phone]
-    @telegram = student_data[:telegram]
-    @email = student_data[:email]
-    @git = student_data[:git]
+    self.telegram = student_data[:telegram]
+    self.email = student_data[:email]
+    self.git = student_data[:git]
+  end
+
+  def self.valid_surname?(surname)
+    surname.match(/\A[А-Яа-яA-Za-z]+\z/)
+  end
+
+  def surname=(surname)
+  	raise ArgumentError, "Некорректно введена фамилия" if !Student.valid_surname?(surname)
+  	@surname=surname
+  end
+
+  def self.valid_name?(name)
+    name.match(/\A[А-Яа-яA-Za-z]+\z/)
+  end
+
+  def name=(name)
+  	raise ArgumentError, "Некорректно введено имя" if !Student.valid_name?(name)
+  	@name=name
+  end
+
+  def self.valid_patronymic?(patronymic)
+    patronymic.match(/\A[А-Яа-яA-Za-z]+\z/)
+  end
+
+  def patronymic=(patronymic)
+  	raise ArgumentError, "Некорректно введена фамилия" if !Student.valid_patronymic?(patronymic)
+  	@patronymic=patronymic
+  end
+
+  def self.valid_id?(id)
+  	id > 0 && id.is_a?(Integer)
+  end
+
+  def id=(id)
+  	raise ArgumentError, "Некорректный ID" if !Student.valid_id?(id)
+  	@id=id
   end
 
   def self.valid_phone?(phone)
@@ -18,8 +53,35 @@ class Student
   end
 
   def phone=(phone)
-  		raise "Некорректный номер телефона" if !Student.valid_phone?(phone)
-  		@phone=phone
+  	raise ArgumentError, "Некорректный номер телефона" if !Student.valid_phone?(phone)
+  	@phone=phone
+  end
+
+  def self.valid_telegram?(telegram)
+  	telegram.match(/\A[A-Za-z0-9_@]+\z/)
+  end
+
+  def telegram=(telegram)
+  	raise ArgumentError, "Некорректно введен Telegram" if !Student.valid_telegram?(telegram)
+  	@telegram=telegram
+  end
+
+  def self.valid_email?(email)
+  	email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/)
+  end
+
+  def email=(email)
+  	raise ArgumentError, "Некорректно введен Email" if !Student.valid_email?(email)
+  	@email=email
+  end
+
+  def self.valid_git?(git)
+  	git.match(/\A[A-Za-z0-9_]+\z/)
+  end
+
+  def git=(git)
+  	raise ArgumentError, "Некорректно введен Git" if !Student.valid_git?(git)
+  	@git=git
   end
 
   def to_s
