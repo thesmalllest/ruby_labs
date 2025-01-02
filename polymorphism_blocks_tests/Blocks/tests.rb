fb34b634fa9f5d14906ff06d01c21521bab803a2
thesmalllest <vikulik.vs.137@gmail.com>
2025-01-02 21:42:59 +0300

    lab 2 | Updated tests

--- polymorphism_blocks_tests/Blocks/tests.rb
+++ polymorphism_blocks_tests/Blocks/tests.rb
@@ -1,20 +1,48 @@
-require_relative 'processing_array'
+require_relative 'processing_array' 
+require 'minitest/autorun'
 
-array = ArrayProcessor.new([1,2,3,4,5])
+class ArrayProcessorTest < Minitest::Test
+  def setup
+    @array = ArrayProcessor.new([1, 2, 3, 4, 5])
+    @chunk_array = ArrayProcessor.new([12, 2, 3, 4, 5, 7])
+  end
 
-puts array.include? {|n| n.even?} # => true
+  def test_include
+    assert_equal true, @array.include? { |n| n.even? }
+    assert_equal false, @array.include? { |n| n > 5 }
+  end
 
-puts array.reduce(1) {|acc, elem| acc * elem} # => 120
-puts array.reduce(10) {|acc, elem| acc + elem} # => 25
+  def test_reduce
+    assert_equal 120, @array.reduce(1) { |acc, elem| acc * elem }
+    assert_equal 25, @array.reduce(10) { |acc, elem| acc + elem }
+  end
 
-puts array.sum {|n| !n.even?} # =>9
+  def test_sum
+    assert_equal 9, @array.sum { |n| !n.even? }
+    assert_equal 6, @array.sum { |n| n.even? }
+  end
 
-puts array.member? {|n| n == 7} # => false
-puts array.member? {|n| n == 1} # => true
+  def test_member
+    assert_equal false, @array.member? { |n| n == 7 }
+    assert_equal true, @array.member? { |n| n == 1 }
+  end
 
-puts array.filter {|n| n.even?} # => 2
-                                # => 4
-                                
-array_for_chunk = ArrayProcessor.new([12,2,3,4,5,7])
+  def test_filter
+    result = @array.filter { |n| n.even? }
+    assert_equal [2, 4], result
 
-puts array_for_chunk.chunk  {|n| n.even?}
+    result = @array.filter { |n| n > 3 }
+    assert_equal [4, 5], result
+  end
+
+  def test_chunk
+    result = @chunk_array.chunk { |n| n.even? }
+    expected = [
+      [true, [12, 2]],
+      [false, [3]],
+      [true, [4]],
+      [false, [5, 7]]
+    ]
+    assert_equal expected, result
+  end
+end
