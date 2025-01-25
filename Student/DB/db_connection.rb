002a58330b9e7a0a2e47f0956b36af6e4043be19
thesmalllest <vikulik.vs.137@gmail.com>
2025-01-25 06:49:58 +0300

    lab 4 | task 3.11
    Added pattern singleton

--- Student/DB/db_connection.rb
+++ Student/DB/db_connection.rb
@@ -1,13 +1,33 @@
 require 'pg'
 
 class DBConnection
-  def self.connection
-    @connection ||= PG.connect(
-      dbname: 'StudentsDataBase',
-      user: 'postgres',
-      password: '123', 
-      host: 'localhost',
-      port: 5432
-    )
+  DEFAULT = {
+    dbname: 'StudentsDataBase',
+    user: 'postgres',
+    password: '123', 
+    host: 'localhost',
+    port: 5432
+  }.freeze
+
+  @instance = nil
+
+  private_class_method :new
+
+  private attr_reader :connection
+
+  def self.instance
+    @instance ||= new
+  end
+
+  def initialize
+    @connection = PG.connect(DEFAULT)
+  end
+
+  def execute_query(query, params = [])
+    connection.exec_params(query, params)
+  end
+
+  def close
+    connection.close
   end
 end
